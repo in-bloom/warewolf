44d2f7df1b0b8dd1045e0975f201a7c25069d3ea
in-bloom <f.ricciardi9@studenti.unipi.it>
2025-12-04 18:08:00 +0100

    Refactor data_loader and import modules; remove unused imports and organize CRUD function imports

---
+++ src/warewolf/data_import/data_loader.py
@@ -0,0 +1,11 @@
+from .db_conn import init_db
+from .crud import (
+    insert_recordings,
+    delete_recordings,
+    get_recordings,
+    insert_sequences,
+    delete_sequences,
+    get_sequences,
+    update_sequence
+)
+
